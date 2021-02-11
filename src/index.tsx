9ff8ee66216fa2296ea7c371f0bb7c745ab68ce9
simon <wanwanzainaer@gmail.com>
2021-02-10 22:20:34 -0800

    Add iframe srcDoc to prevent user change the html

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { fetchPlugin } from './fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -23,6 +23,8 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -33,14 +35,30 @@ const App = () => {
         global: 'window',
       },
     });
-    console.log(result);
-    setCode(result.outputFiles[0].text);
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+  <html> 
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          try{
+            eval(event.data);
+          } catch (e) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + e + '</div>';
+            console.error(e);
+          }
+        },false)
+      </script>
+    </body>
+  </html>
+  `;
 
   return (
     <div>
@@ -51,8 +69,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe sandbox="" src="/test.html" />
+      <iframe
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+        title="preview"
+      />
     </div>
   );
 };
