051ca6fd7bc1c8ec205936a3ed03a6892d192d8a
simon <wanwanzainaer@gmail.com>
2021-02-19 10:48:43 -0800

    finished the bundles reducer

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import { cellsReducer } from './cellsReducer';
-
+import { bundlesReducer } from './bundlesReducer';
 export const reducers = combineReducers({
   cells: cellsReducer,
+  bundles: bundlesReducer,
 });
 
 export type RootState = ReturnType<typeof reducers>;
